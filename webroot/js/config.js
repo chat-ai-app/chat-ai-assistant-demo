4409eec1dd399073b9cec8e4230abdaec2764492
Marc Hadfield <marc@vital.ai>
2023-12-09 11:07:09 -0500

    sync

---
+++ webroot/js/config.js
@@ -0,0 +1,26 @@
+//GLOBAL INJECTED CONFIG
+var APP_ID = "chat-saas";
+	        
+var HTTPS_URL = "https://www.chat.ai/demo.html";
+	    	
+// var WEBSITE_URL = "${WEBSITE_URL}";
+	    	
+// var WEBAPP_CONNECTED = ${WEBAPP_CONNECTED};
+	    	
+// var WEBAPP_URL = "${WEBAPP_URL}";
+	    	
+// var SAAS_SERVER_URL = "${SAAS_SERVER_URL}";
+	    	
+// var EVENTBUS_URL = "${EVENTBUS_URL}";
+	    	
+// var SESSION_DOMAIN = "${SESSION_DOMAIN}";
+	    	
+// var COOKIE_PREFIX = "${COOKIE_PREFIX}";
+	    	
+// var COOKIE_SECURE = "${COOKIE_SECURE}" == 'true';
+	    	
+// var ACCOUNT_ID_MODE = "${ACCOUNT_ID_MODE}";
+	    	
+// var ACCOUNT_ID = "${ACCOUNT_ID}";
+
+// var ROLES_CHECK_ENABLED = "${ROLES_CHECK_ENABLED}" == 'true';
