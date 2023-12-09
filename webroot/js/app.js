4012fffc16d3ffe763671e185a9a8e3d3bb265ab
Marc Hadfield <marc@vital.ai>
2023-12-09 15:43:15 -0500

    sync

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -78,6 +78,9 @@ class HaleyWakeWordApp {
  
 window.HaleyWakeWordApp = HaleyWakeWordApp;
 
-const app = new HaleyWakeWordApp();
+const wakeWordApp = new HaleyWakeWordApp();
 
-app.render();
+wakeWordApp.render();
+
+
+
